185e269ac44409b68ad0c706062a429b565d4ad0
tntexplodes101 <33296248+tntexplodes101@users.noreply.github.com>
2018-10-18 10:41:27 -0400

    Change to code comments and readability

--- Arduino/buzzer_code_modified/buzzer_code_modified.ino
+++ Arduino/buzzer_code_modified/buzzer_code_modified.ino
@@ -2,17 +2,20 @@
 const int timeToAns = 4000; //in millis
 const int timeBuzzerOn = 700; //in millis
 
-//const vars
+/* constant vars */
+// GPIO pins for buttons
+const int masterSwitchPin = 2;
 const int player1Pin = 7;
 const int player2Pin = 9;
 const int player3Pin = 11;
+// LED's corresponding to user buttons
 const int led1 = 8;
 const int led2 = 10;
 const int led3 = 12;
-const int masterSwitchPin = 2;
+
 const int led = 13;
 
-//changing vars
+/*other vars*/
 int player1 = 0; //current state of player 1 button
 int player2 = 0; //current state of player 2 button
 int player3 = 0; //current state of player 3 button
@@ -36,10 +39,14 @@ void setup() {
 }
 
 
-
+void LEDState(int state)
+{
+  digitalWrite(led1, state);
+  digitalWrite(led2, state);
+  digitalWrite(led3, state);
+}
 //start of loop
 void loop() {
-  //Serial.print("start of loop, ");
   masterSwitch = digitalRead(masterSwitchPin);
   Serial.print("time im millis: ");
   Serial.println(millis());
@@ -48,25 +55,19 @@ void loop() {
   //tests if player answered in timeframe and 
   if (timeSincePress >= timeToAns && timeSincePress < timeBuzzerOff && msPressed == HIGH) {
 
-    digitalWrite(led1, HIGH);
-    digitalWrite(led2, HIGH);
-    digitalWrite(led3, HIGH);
+    LEDState(HIGH);
   }
 
   if (timeSincePress >= timeBuzzerOff && msPressed == HIGH) {
 
-    digitalWrite(led1, LOW);
-    digitalWrite(led2, LOW);
-    digitalWrite(led3, LOW);
+    LEDState(LOW);
+    
   }
 
   //makes sure that even if master switch is held, players can still ring in.
-  if (masterSwitch == HIGH && prevMasterSwitch != masterSwitch && player1 == LOW && player2 == LOW && player3 == LOW) {
-
+  if (masterSwitch == HIGH && (prevMasterSwitch != masterSwitch) && (player1 == LOW && player2 == LOW && player3 == LOW)) {
     digitalWrite(led, HIGH);
-    digitalWrite(led1, LOW);
-    digitalWrite(led2, LOW);
-    digitalWrite(led3, LOW);
+    LEDState(LOW);
     Serial.print("Who buzzed first: ");
     while (player1 == LOW && player2 == LOW && player3 == LOW) {
       player1 = digitalRead(player1Pin);
@@ -99,7 +100,6 @@ void loop() {
   }
   
   prevMasterSwitch = masterSwitch;
-  //Serial.println("end of loop");
 
 }
 
